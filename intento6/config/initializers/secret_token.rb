0b3d94ef703c989cc02d882644cad77d5460e349
SKL <ludwig.andres@gmail.com>
2013-07-19 00:05:19 -0400

    Initialize commit

---
+++ intento6/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Intento6::Application.config.secret_key_base = 'f368a2433b7f2327b8911e7b7347e185ae420f5c28d435a671ae2db985bbc318744b804a357ff94806d7e5d717ffe8a3167ee1678711cf91a9004a2ce8c76175'
